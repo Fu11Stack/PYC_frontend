25c20cd71eb2c8c22a361ed5d96c2156798247bb
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2023-01-26 17:27:27 -0800

    Add files via upload

---
+++ src/components/Public.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+const Public = () => {
+ 
+   const content = (
+   <section className="public">
+       <header>
+           <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+       </header>
+       <main className="public__main">
+           <p>Pruneyard Cinemas Login Portal</p>
+           <address className="public__addr">
+               PruneYard Cinema's<br />
+               555 Foo Drive<br />
+               Foo City, CA 12345<br />
+               <a href="tel:+15555555555">(555) 555-5555</a>
+           </address>
+           <br />
+           <p>PYC Inc</p>
+       </main>
+       <footer>
+           <Link to="/login">Employee Login</Link>
+       </footer>
+   </section>
+   )
+   return content 
+
+}
+
+export default Public
