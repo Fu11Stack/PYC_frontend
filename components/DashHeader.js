ea6a4e96c427f8df4959d87ae76d2994f0517a2e
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2023-01-28 18:48:40 -0800

    Add files via upload

---
+++ components/DashHeader.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+const  DashLayout = () => {
+ const content = (
+  <header className="dash-header">
+  <div className="dash-header__container">
+      <Link to="/dash">
+          <h1 className="dash-header__title">Pruneyard Cinemas</h1>
+      </Link>
+      <nav className="dash-header__nav">
+          {/* add nav buttons later */}
+      </nav>
+  </div>
+</header>
+ ) 
+ return content
+}
+
+export default DashLayout
