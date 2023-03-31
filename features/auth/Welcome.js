170e126d6e1297d860fb76c31395c2276018c642
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2023-03-30 21:43:18 -0700

    Add files via upload

--- features/auth/Welcome.js
+++ features/auth/Welcome.js
@@ -2,24 +2,29 @@ import { Link } from "react-router-dom"
 
 const Welcome = () => {
 
- const date = new Date()
- const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
+    const date = new Date()
+    const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
 
- const content = (
-     <section className="welcome">
+    const content = (
+        <section className="welcome">
 
-         <p>{today}</p>
+            <p>{today}</p>
 
-         <h1>Welcome!</h1>
+            <h1>Welcome!</h1>
 
-         <p><Link to="/dash/notes">Accounts and Passwords</Link></p>
+            <p><Link to="/dash/notes">Accounts and Passwords</Link></p>
 
-         <p><Link to="/dash/users">View User Settings</Link></p>
+            <p><Link to="/dash/users">View User Settings</Link></p>
 
-     </section>
- )
+            <p><Link to="/dash/notes/new">Add New Account & Password</Link></p>
 
- return content
+            <p><Link to="/dash/users/new">Add New User</Link></p>
+
+
+        </section>
+    )
+
+    return content
 
 }
 
---
+++ features/notes/EditNoteForm.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react"
+import { usesNavigate } from "react-router-dom"
+import { useAddNewNoteMutation } from "./notesApiSlice"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSave } from "@fortawesome/free-solid-svg-icons"
+
+const EditNoteForm = ({ users }) => {
+ return (
+  <div>EditNoteForm</div>
+ )
+}
+
+export default EditNoteForm
