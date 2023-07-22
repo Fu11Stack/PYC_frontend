e01f93783fc5c0284f4b5344df7e2bc15a8d8a2b
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2023-07-21 23:34:25 -0700

    Add files via upload

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,24 +1,24 @@
 const jwt = require('jsonwebtoken')
 
 const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.authorization
-  
-  if (!authHeader?.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Unauthorized'})
-  }
+    const authHeader = req.headers.authorization || req.headers.Authorization
 
-  const token = authHeader.split(' ')[1]
+    if (!authHeader?.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
 
-  jwt.verify(
-   token,
-   process.env.ACCESS_TOKEN_SECRET,
-   (err, decoded) => {
-    if(err) return res.status(403).json({ message: 'Forbidden '})
-    req.user = decoded.UserInfo.username
-    req.roles = decoded.UserInfo.roles
-    next()
-   }
-  )
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.status(403).json({ message: 'Forbidden' })
+            req.user = decoded.UserInfo.username
+            req.roles = decoded.UserInfo.roles
+            next()
+        }
+    )
 }
 
-modules.export = verifyJWT
+module.exports = verifyJWT 
